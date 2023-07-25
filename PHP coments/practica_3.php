56d4f575df6fd16bec2d82aa3dbb7d1c4006f9e7
ValeriaLeonH <valerialeon837@gmail.com>
2023-07-25 10:52:05 -0400

    guardar

---
+++ PHP coments/practica_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    //Tu puedes incluso comentar partes del codigo para que no se ejecuten 
+
+    $x = 5 /* +15 */ +5;
+    echo $x; 
+  ?>
+</body>
+</html>
---
+++ sintaxis/practica_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        ECHO "Hola mundo!<br>";
+        echo "Hola mundo!<br>";
+        EcHo "Hola mundo!<br>";
+    ?>
+</body>
+</html>
